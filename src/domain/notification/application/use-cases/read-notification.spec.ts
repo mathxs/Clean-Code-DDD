4a25de4b6a327c636b4a8bcfd02d034907cb746d
mathxs <matheusdsantosp@gmail.com>
2024-07-08 09:40:00 -0300

    novo teste: ler notificacao de outro usuario

--- src/domain/notification/application/use-cases/read-notification.spec.ts
+++ src/domain/notification/application/use-cases/read-notification.spec.ts
@@ -1,6 +1,8 @@
 import { InMemoryNotificationsRepository } from 'test/repositories/in-memory-notification-repository'
 import { ReadNotificationUseCase } from './read-notification'
 import { makeNotification } from 'test/factories/make-notification'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
 
 let inMemoryNotificationsRepository: InMemoryNotificationsRepository
 let sut: ReadNotificationUseCase
@@ -24,5 +26,19 @@ describe('Read Notification', () => {
     expect(inMemoryNotificationsRepository.items[0].readAt).toEqual(
       expect.any(Date),
     )
+  })
+
+  test('should not be able to read a notification from another user', async () => {
+    const notification = makeNotification(
+      { recipientId: new UniqueEntityID('recipient-1') },
+      new UniqueEntityID('123'),
+    )
+    await inMemoryNotificationsRepository.create(notification)
+    const result = await sut.execute({
+      recipientId: 'recipient-2',
+      notificationId: notification.id.toString(),
+    })
+    expect(result.isRight()).toBe(false)
+    expect(result.value).toBeInstanceOf(NotAllowedError)
   })
 })
